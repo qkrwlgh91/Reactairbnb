89b9255950817260bc41819df9818ed8246c6e1e
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-07-12 06:13:55 +0900

    scss standard structure setting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,21 +3,59 @@ import React from "react";
 const Footer = () => {
     return (
         <div className="footerBox">
-
+            <div className="footerInfoBox">
+                <ul>
+                    <li>소 개</li>
+                    <li>에어비앤비 이용 방법</li>
+                    <li>다양성 및 소속감</li>
+                    <li>접근성</li>
+                    <li>신뢰와 안전</li>
+                    <li>Aribnb Citizen</li>
+                    <li>올림픽</li>
+                    <li>뉴스룸</li>
+                </ul>
+                <ul>
+                    <li>커뮤니티</li>
+                    <li>에어비앤비 매거진</li>
+                    <li>에어비앤비 어소시에이트</li>
+                    <li>에어비앤비 비즈니스 프로그램</li>
+                    <li>친구 초대하기</li>
+                    <li>채용정보</li>
+                </ul>
+                <ul>
+                    <li>호스팅하기</li>
+                    <li>숙소 호스팅</li>
+                    <li>온라인 체험 호스팅하기</li>
+                    <li>브라이언 체스키 CEO의 메시지</li>
+                    <li>책임감 있는 호스팅</li>
+                    <li>Open Homes</li>
+                    <li>자료 센터</li>
+                    <li>커뮤니티 센터</li>
+                </ul>
+                <ul>
+                    <li>에어비앤비 지원</li>
+                    <li>코로나19 관련 업데이트</li>
+                    <li>도움말 센터</li>
+                    <li>예약 취소 옵션</li>
+                    <li>에어비앤비 이웃 민원 지원</li>
+                </ul>
+            </div>
+            <hr/>
+            <div className="footerCopright">
                 <ul>
                     <li>&copy;2020 AirBnb, Inc. All rights reserved</li>
-                    <li>개인정보 처리방침</li>
-                    <li>이용약관 </li>
-                    <li>사이트맵 </li>
-                    <li>한국의 변경된 환불 정책</li>
+                    <li>&middot; 개인정보 처리방침</li>
+                    <li>&middot; 이용약관</li>
+                    <li>&middot; 사이트맵</li>
+                    <li>&middot; 한국의 변경된 환불 정책</li>
                 </ul>
-
                 <ul>
                     <li>한국어(KR)</li>
                     <li>&#8361; KRW</li>
                     <li>사진 사진 사진 사진 사진</li>
                 </ul>
             </div>
+        </div>
     )
 }
 
