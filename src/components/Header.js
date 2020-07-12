9a94fdef63cd703a9b8a96fc2bfe6b9211765ca9
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-07-13 03:26:30 +0900

    Header work ver.1

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,19 @@ import log from '../scss/img/airbnbLog.png';
 const Header = () => {
     return (
         <div className="headerBox">
-            <Link to="/"><img src={log} alt="airbnb homepage log"/></Link>
-            <ul>
-                <li>숙소 호스트 되기</li>
-                <li>체험 호스팅하기</li>
-                <li>도움말</li>
-                <li><Link to="/Login">로그인</Link></li>
-                <li><Link to="/Login/smart">Login</Link></li>
-                <li><Link to="/SignIn">회원가입</Link></li>
-            </ul>
+            <div className="airbnbLog">
+                <Link to="/"><img src={log} alt="airbnb homepage log"/></Link>
+            </div>
+            <div className="headerList">
+                <ul>
+                    <li>사진</li>
+                    <li>Host your home</li>
+                    <li>Host an experience</li>
+                    <li>Help</li>
+                    <li><Link to="/Login">Log in</Link></li>
+                    <li><Link to="/SignIn">Sing up</Link></li>
+                </ul>
+            </div>
             <hr/>
         </div>
     )
